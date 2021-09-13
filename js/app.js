03433e7756b67898c0c1dad229b2f6822e67510a
Rasedul <raselr699@gmail.com>
2021-09-13 11:50:00 +0600

    cart body color and website name change

--- js/app.js
+++ js/app.js
@@ -11,13 +11,13 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product p-3">
       <div>
     <img class="product-image" src='${image}'></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4 class="my-3">${product.title}</h4>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <h4>Price: $ ${product.price}</h4>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
